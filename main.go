1cb2456caa4dece18c3ae0eaabd8fe22d71ab1a6
이익태 <magutae@efit.co.kr>
2021-12-03 17:09:37 +0900

    5.1 Rendering Templates

--- main.go
+++ main.go
@@ -2,67 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 
 	"github.com/magutae/kumayacoin/blockchain"
 )
 
-// import (
-// 	"crypto/sha256"
-// 	"fmt"
-// )
+const port string = ":4000"
 
-// type block struct {
-// 	data     string
-// 	hash     string
-// 	prevHash string
-// }
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
 
-// type blockchain struct {
-// 	blocks []block
-// }
-
-// func (b *blockchain) getLastHash() string {
-// 	if len(b.blocks) > 0 {
-// 		return b.blocks[len(b.blocks)-1].hash
-// 	}
-// 	return ""
-// }
-
-// func (b *blockchain) addBlcok(data string) {
-// 	newBlock := block{data, "", b.getLastHash()}
-// 	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-// 	newBlock.hash = fmt.Sprintf("%x", hash)
-
-// 	b.blocks = append(b.blocks, newBlock)
-// }
-
-// func (b *blockchain) listBlocks() {
-// 	for _, block := range b.blocks {
-// 		fmt.Printf("Data: %s\n", block.data)
-// 		fmt.Printf("Hash: %s\n", block.hash)
-// 		fmt.Printf("Prev Hash: %s\n\n", block.prevHash)
-// 	}
-
-// }
+func home(rw http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
+}
 
 func main() {
-	// genesisBlock := block{"Genesis Block", "", ""}
-	// hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
-	// hexHash := fmt.Sprintf("%x", hash)
-	// genesisBlock.hash = hexHash
-	// fmt.Println(genesisBlock)
-	// chain := blockchain{}
-	// chain.addBlcok("Genesis Block")
-	// chain.addBlcok("Second Block")
-	// chain.addBlcok("Third Block")
-	// chain.listBlocks()
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
-	}
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
