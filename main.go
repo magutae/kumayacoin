bd213af9ffb0c5e2fe82eefb57d1f7e3880976df
magutae <magutae@gmail.com>
2022-01-10 23:14:39 +0900

    6.0 Setup

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import explorer "github.com/magutae/kumayacoin/explorer/templates"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/magutae/kumayacoin/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentaion",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
