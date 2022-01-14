d8b80bce0e85a4bfa6a1f1745642b28a8b3404f7
magutae <magutae@gmail.com>
2022-01-14 18:01:26 +0900

    6.5 Gorilla Mux

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	explorer "github.com/magutae/kumayacoin/explorer/templates"
 	"github.com/magutae/kumayacoin/rest"
 )
 
 func main() {
-	go explorer.Start(3000)
 	rest.Start(4000)
 }
--- rest/rest.go
+++ rest/rest.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/magutae/kumayacoin/blockchain"
 	"github.com/magutae/kumayacoin/utils"
 )
@@ -61,11 +62,18 @@ func blocks(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func block(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+}
+
 func Start(aPort int) {
-	handler := http.NewServeMux()
+	handler := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
-	handler.HandleFunc("/", documentation)
-	handler.HandleFunc("/blocks", blocks)
+	handler.HandleFunc("/", documentation).Methods("GET")
+	handler.HandleFunc("/blocks", blocks).Methods("GET", "POST")
+	handler.HandleFunc("/blocks/{id:[0-9]+}", block).Methods("GET")
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, handler))
 }
