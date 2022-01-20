1f321098b1320a8ee1ce517c699137c7c9b31f55
magutae <magutae@gmail.com>
2022-01-20 23:34:27 +0900

    7.2 FlagSet

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"github.com/magutae/kumayacoin/rest"
+	"flag"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to kumayacoin\n\n")
+	fmt.Printf("Please use the following commands:\n\n")
+	fmt.Printf("explorer:	Start the HTML Explorer\n")
+	fmt.Printf("rest:		Start the REST API (recommended)\n")
+	os.Exit(0)
+}
+
 func main() {
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+
+	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Printf("Start Explorer")
+	case "rest":
+		rest.Parse(os.Args[2:])
+	default:
+		usage()
+	}
+
+	if rest.Parsed() {
+		fmt.Println(*portFlag)
+		fmt.Println("Start server")
+	}
 }
