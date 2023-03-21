74be913642a8a30d6a21f86a315cb79687594368
이익태 <magutae@gmail.com>
2023-03-21 10:26:28 +0900

    11.3 Verifying Messages

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,15 +1,5 @@
 package wallet
 
-import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"encoding/hex"
-	"fmt"
-
-	"github.com/magutae/kumayacoin/utils"
-)
-
 // 1) hash the msg
 // "message" -> hash(x) -> "hashed_message"
 
@@ -22,21 +12,12 @@ import (
 // 4) verify
 // ("hashed_message", + "segnature" + publicKey) -> true / false
 
+const (
+	signature     string = "18bcb6862fdf459c33021091f521fd37a74d62006488df2fde65d79bbcdf98ba85601f364fbc3942b2cc1961ef4b866e4bfccbfab51b422473991f482b6b3adb"
+	privateKey    string = "30770201010420cca45b6a3f0209b12e5c3f67c43fe2c9b0555e4b7a1f4b32ff1a8d7680359564a00a06082a8648ce3d030107a14403420004db99c4dc07d960b2793745290be09be23be58fab275a9b5018b33fbc15d73a18d8635d9f189db5a9d0915b0a6a1a9ad663eea5819eb44f1ce65703f2f8e48ae3"
+	hashedMessage string = "1c5863cd55b5a4413fd59f054af57ba3c75c0698b3851d70f99b8de2d5c7338f"
+)
+
 func Start() {
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	utils.HandleErr(err)
 
-	message := "i love you"
-	hashedMessage := utils.Hash(message)
-
-	fmt.Println(hashedMessage)
-
-	hashAsBytes, err := hex.DecodeString(hashedMessage)
-	utils.HandleErr(err)
-
-	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashAsBytes)
-	utils.HandleErr(err)
-
-	ok := ecdsa.Verify(&privateKey.PublicKey, hashAsBytes, r, s)
-	fmt.Println("ok:", ok)
 }
