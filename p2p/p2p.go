1555849f5f16fc3210a2ea1d18dceac99bb37b9b
이익태 <magutae@gmail.com>
2023-04-19 10:49:29 +0900

    12.8 Connections

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,13 +1,13 @@
 package p2p
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 	"github.com/magutae/kumayacoin/utils"
 )
 
+var conns []*websocket.Conn
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
@@ -15,10 +15,18 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 		return true
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
+	conns = append(conns, conn)
 	utils.HandleErr(err)
 	for {
 		_, p, err := conn.ReadMessage() // blocking
-		utils.HandleErr(err)
-		fmt.Printf("%s\n", p)
+		if err != nil {
+			conn.Close()
+			break
+		}
+		for _, aConn := range conns {
+			if aConn != conn {
+				utils.HandleErr(aConn.WriteMessage(websocket.TextMessage, p))
+			}
+		}
 	}
 }
