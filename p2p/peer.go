5d15977a48485bac4ca40aa32db06577821c77f1
이익태 <magutae@gmail.com>
2023-04-20 11:03:36 +0900

    12.15 Cleanup - golang defer

--- p2p/peer.go
+++ p2p/peer.go
@@ -9,14 +9,51 @@ import (
 var Peers map[string]*peer = make(map[string]*peer)
 
 type peer struct {
-	conn *websocket.Conn
+	key     string
+	address string
+	port    string
+	conn    *websocket.Conn
+	inbox   chan []byte
+}
+
+func (p *peer) close() {
+	p.conn.Close()
+	delete(Peers, p.key)
+}
+
+func (p *peer) read() {
+	defer p.close()
+	for {
+		_, m, err := p.conn.ReadMessage() // blocking
+		if err != nil {
+			break
+		}
+		fmt.Printf("%s", m)
+	}
+}
+
+func (p *peer) write() {
+	defer p.close()
+	for {
+		m, ok := <-p.inbox // blocking
+		if !ok {
+			break
+		}
+		p.conn.WriteMessage(websocket.TextMessage, m)
+	}
 }
 
 func initPeer(conn *websocket.Conn, address, port string) *peer {
-	p := &peer{
-		conn,
-	}
 	key := fmt.Sprintf("%s:%s", address, port)
+	p := &peer{
+		key:     key,
+		address: address,
+		port:    port,
+		conn:    conn,
+		inbox:   make(chan []byte),
+	}
 	Peers[key] = p
+	go p.read()
+	go p.write()
 	return p
 }
